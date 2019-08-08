f81f5e6a691cecfbe08bae61225fe2c2c71886ce
kraxarn <me@kraxarn.com>
2019-08-09 00:17:31 +0200

    Ignore start screen when already selected

--- lib/page/start_page.dart
+++ lib/page/start_page.dart
@@ -7,18 +7,28 @@ import 'package:shared_preferences/shared_preferences.dart';
 /// Starting page for choosing school
 class StartPage extends StatelessWidget
 {
+	_pushMain(BuildContext context)
+	{
+		Navigator.of(context).pushReplacementNamed("/main");
+	}
+	
 	_saveSchool(BuildContext context, String schoolId) async
 	{
 		// Save to preferences and go to main page
 		SharedPreferences.getInstance().then((prefs) {
 			prefs.setString("school", schoolId);
-			Navigator.of(context).pushReplacementNamed("/main");
+			_pushMain(context);
 		});
 	}
 	
 	@override
 	Widget build(BuildContext context)
 	{
+		SharedPreferences.getInstance().then((prefs) {
+			if (prefs.getString("school") != null)
+				_pushMain(context);
+		});
+		
 		return Scaffold(
 			appBar: AppBar(
 				title: Text("Select School"),
