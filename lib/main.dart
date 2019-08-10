3997a4615f7411a884724bd7fab5fa8ae80c0ba8
kraxarn <me@kraxarn.com>
2019-08-10 18:08:13 +0200

    Load theme on startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-import 'page/start_page.dart';
 import 'page/main_page.dart';
-import 'page/settings_page.dart';
+import 'page/start_page.dart';
 
-void main() => runApp(MyApp());
+void main()
+{
+	SharedPreferences.getInstance().then((prefs) {
+		runApp(MyApp(
+			prefs.getString("theme") == "dark"
+				? ThemeData.dark() : ThemeData.light()
+		));
+	});
+}
 
 class MyApp extends StatelessWidget
 {
+	final ThemeData _baseTheme;
+	
+	MyApp(this._baseTheme);
+	
 	@override
 	Widget build(BuildContext context)
 	{
 		return MaterialApp(
 			title: "KronoX",
-			theme: ThemeData.dark().copyWith(
+			theme: _baseTheme.copyWith(
 				primaryColor: Colors.orange[500],
 				primaryColorDark: Colors.orange[700],
 				accentColor: Colors.deepOrangeAccent
@@ -21,8 +33,7 @@ class MyApp extends StatelessWidget
 			home: StartPage(),
 			routes: {
 				"/start":    (context) => StartPage(),
-				"/main":     (context) => MainPage(),
-				"/settings": (context) => SettingsPage()
+				"/main":     (context) => MainPage()
 			},
 		);
 	}
