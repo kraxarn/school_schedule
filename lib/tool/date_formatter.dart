374776360553bc94fb7b3ddacb901ecdd7006945
kraxarn <me@kraxarn.com>
2019-08-23 17:32:57 +0200

    Make add leading public

--- lib/tool/date_formatter.dart
+++ lib/tool/date_formatter.dart
@@ -2,15 +2,15 @@
 class DateFormatter
 {
 	/// Adds a leading zero if < 10
-	static String _addLeading(int value) =>
+	static String addLeading(int value) =>
 		value < 10 ? "0$value" : "$value";
 	
 	/// Formats as YYYY-MM-DD
 	static String asFullDate(DateTime date) =>
-		"${date.year}-${_addLeading(date.month)}-${_addLeading(date.day)}";
+		"${date.year}-${addLeading(date.month)}-${addLeading(date.day)}";
 	
 	/// Formats as YYYY-MM-DD HH:MM
 	static String asFullDateTime(DateTime date) =>
 		"${asFullDate(date)} "
-			"${_addLeading(date.hour)}:${_addLeading(date.minute)}";
+			"${addLeading(date.hour)}:${addLeading(date.minute)}";
 }
